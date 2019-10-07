479d39fff29b46f6100156b4d83ecb831b494e43
EilonwyLlyr <48164225+EilonwyLlyr@users.noreply.github.com>
2019-10-06 19:31:13 -0700

    Update ForkCompress.c

--- ForkCompress.c
+++ ForkCompress.c
@@ -5,14 +5,14 @@
 #include <sys/wait.h>
 
 void child(char fp1[], char fp2[]){
-
+    //executes MyCompress and sends the arguments from the command line to it.
     execl("./MyCompress", "./MyCompress", fp1, fp2, NULL);
     printf("child finished\n");
-
+    exit(1);
 }
 
 int main(int argc, char* argv[]){
-
+    //checks to see if arguments are passes in the command line
     if(argc == 1){
         printf("No arguments passed\n");
     }
@@ -20,16 +20,20 @@ int main(int argc, char* argv[]){
         printf("incuficent arguemnts\n");
     }
     else if(argc == 3){
-
+        //creates a pid and forks
         pid_t pid;
         pid = fork();
+        //checks for erros
         if(pid < 0){
             printf("Error: fork failed.\n");
+            exit(1);
         }
         else if(pid == 0){
+            //child sends information into child function
             child(argv[1], argv[2]);
         }
         else{
+            //parent waits for child
             wait(&pid);
             printf("This is Parent\n");
         }
