ae5c46d8fff0591489e46f637e871ced1c598bad
EilonwyLlyr <48164225+EilonwyLlyr@users.noreply.github.com>
2019-10-06 19:30:48 -0700

    Update MinShell.c

--- MinShell.c
+++ MinShell.c
@@ -5,12 +5,8 @@
 #include <sys/wait.h>
 #include <string.h>
 
-void child(char command[]){
-    char *cmd = strtok(command, "\n");
-    char *arg[2];
-    arg[0] = cmd;
-    arg[1] = NULL;
-    execvp(cmd, arg);
+void child(char* arg[]){
+    execvp(arg[0], arg);
 }
 
 int main(int argc, char *argv[]){
@@ -19,22 +15,34 @@ int main(int argc, char *argv[]){
     int *status;
 
     pid_t pid;
+    //the start of the shell
     while (strcmp("exit\n", command) != 0){
         printf("enter:");
-
+        //gets the input from the user and places it in command
         fgets(command, 1024, stdin);
+        //fgets keeps the \n from the user input
         if(strcmp("exit\n", command) != 0){
+            //removes the \n  from the input
+            char *cmd = strtok(command, "\n");
+            char *arg[2];
+            //sets up the inputs from the execvp
+            arg[0] = cmd;
+            arg[1] = NULL;
+            
             pid = fork();
             if (pid == -1){
                 printf("Error");
             }
             else if(pid == 0){
-                child(command);
+                //child sends the command and executes it.
+                child(arg);
                 exit(0);
             }
             else{
+                //waits for child
                 waitpid(pid, status, WUNTRACED);
             }
         }
     }
+    return 0;
 }
